bdd9be7c38270fadc379824be931466e542d5a46
Anastasiya Balko <yuanchik2024@gmail.com>
2025-05-31 20:32:39 +0300

    screen display

---
+++ app/src/main/java/ru/yuanchik/example/onlineshop/adapter/PopularAdapter.java
@@ -0,0 +1,70 @@
+package ru.yuanchik.example.onlineshop.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
+
+import java.util.ArrayList;
+
+import ru.yuanchik.example.onlineshop.databinding.ViewholderPupListBinding;
+import ru.yuanchik.example.onlineshop.domain.PopularDomain;
+
+public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.viewholder> {
+    ArrayList<PopularDomain> items;
+    Context context;
+    ViewholderPupListBinding binding;
+
+    public PopularAdapter(ArrayList<PopularDomain> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public PopularAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ViewholderPupListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        context = parent.getContext();
+        return new viewholder(binding);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull PopularAdapter.viewholder holder, int position) {
+        binding.titleTxt.setText(items.get(position).getTitle());
+        binding.feeTxt.setText("$"+items.get(position).getPrice());
+        binding.scoreTxt.setText(""+items.get(position).getScore());
+
+        int drawableResourced=holder.itemView.getResources().getIdentifier(items.get(position).getPicUrl(),
+                "drawable",holder.itemView.getContext().getPackageName());
+
+        Glide.with(context)
+                .load(drawableResourced)
+                .transform(new GranularRoundedCorners(30,30,0,0))
+                .into(binding.pic);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        public viewholder(ViewholderPupListBinding binding) {
+            super(binding.getRoot());
+        }
+    }
+}
